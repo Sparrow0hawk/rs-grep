177f3f15ff263b7a373bef3354552be640dc05fe
Sparrow0hawk <alexjcoleman@gmail.com>
2022-08-14 16:16:21 +0100

    refactor moving code into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
+use rs_grep::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
-
-struct Config {
-    query: String,
-    file_path: String,
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,30 +12,9 @@ fn main() {
     println!("Searching for '{}'", config.query);
     println!("In file '{}'", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = rs_grep::run(config) {
         println!("Application error: {e}");
 
         process::exit(1);
     }
-}
-
-impl Config {
-    // implement a new function for the config
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n {contents}");
-
-    Ok(())
 }
